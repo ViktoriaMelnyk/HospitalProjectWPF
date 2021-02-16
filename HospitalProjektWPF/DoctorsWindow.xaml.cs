2fcead75d55e6db7d7e19c61616e40deb80f5cab
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-02-16 22:11:21 +0100

    DoctorsWindow almost done

--- HospitalProjektWPF/DoctorsWindow.xaml.cs
+++ HospitalProjektWPF/DoctorsWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Data.Entity;
 
 namespace HospitalProjektWPF
 {
@@ -19,9 +21,13 @@ namespace HospitalProjektWPF
     /// </summary>
     public partial class DoctorsWindow : Window
     {
+        Doctors doctor = new Doctors();
+
         public DoctorsWindow()
         {
             InitializeComponent();
+            dostorsList();
+
         }
 
         private void MainMenu_btn_Click(object sender, RoutedEventArgs e)
@@ -30,5 +36,53 @@ namespace HospitalProjektWPF
             this.Visibility = Visibility.Hidden;
             objMainMenu.Show();
         }
+
+        void Clear()
+        {
+            DocName_input.Text = DocLastName_input.Text = DocPESEL_input.Text = DocPhone_input.Text = DocEmail_input.Text = IdSpec_input.Text = "";
+        }
+
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            Clear();
+        }
+
+        private void AddDoc_btn_Click(object sender, RoutedEventArgs e)
+        {   
+
+            doctor.FirstName = DocName_input.Text;
+            doctor.LastName = DocLastName_input.Text;
+            doctor.PESEL = DocPESEL_input.Text;
+            doctor.phoneNb = DocPhone_input.Text;
+            doctor.mail = DocEmail_input.Text;
+            var idspec = IdSpec_input.Text;
+            int idValue = int.Parse(idspec);
+            doctor.specializations = idValue;
+
+            using (PrzychodniaEntities db = new PrzychodniaEntities())
+            {
+                db.Doctors.Add(doctor);
+                db.SaveChanges();
+            }
+            MessageBox.Show("Dodano nowego lekarza");
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource przychodniaEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("przychodniaEntitiesViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // przychodniaEntitiesViewSource.Źródło = [ogólne źródło danych]
+        }
+        private void dostorsList()
+        {
+            using (PrzychodniaEntities db = new PrzychodniaEntities())
+            {
+                doctorsDataGrid.ItemsSource = db.Doctors.ToList();
+            }
+              
+        }
+
     }
 }
