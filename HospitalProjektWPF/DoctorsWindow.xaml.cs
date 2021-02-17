969f05d9f8d490ee18370b098f7d96f077eb5955
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-02-17 18:15:40 +0100

    Added possibility to add new doctors and patients

--- HospitalProjektWPF/DoctorsWindow.xaml.cs
+++ HospitalProjektWPF/DoctorsWindow.xaml.cs
@@ -21,6 +21,7 @@ namespace HospitalProjektWPF
     /// </summary>
     public partial class DoctorsWindow : Window
     {
+       
         Doctors doctor = new Doctors();
 
         public DoctorsWindow()
@@ -28,6 +29,89 @@ namespace HospitalProjektWPF
             InitializeComponent();
             dostorsList();
 
+        }
+        void Clear()
+        {
+            DocName_input.Text = DocLastName_input.Text = DocPESEL_input.Text = DocPhone_input.Text = DocEmail_input.Text = IdSpec_input.Text = "";
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            System.Windows.Data.CollectionViewSource przychodniaEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("przychodniaEntitiesViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // przychodniaEntitiesViewSource.Źródło = [ogólne źródło danych]
+        }
+
+      
+
+        private void AddDoc_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new PrzychodniaEntities();
+            InvalidData_box.Text = "";
+            var doctorsTable = db.Doctors;
+            var specTable = db.Specializations;
+            string name = DocName_input.Text;
+            if (name == "")
+            {
+                InvalidData_box.Text = "Proszę podać imę!";
+                return;
+            }
+            string lastName = DocLastName_input.Text;
+            if (lastName == "")
+            {
+                InvalidData_box.Text = "Proszę podać nazwisko!";
+                return;
+            }
+            string pesel = DocPESEL_input.Text;
+            if (pesel == "" || pesel.Length != 11)
+            {
+                InvalidData_box.Text = "Proszę podać prawidłowy PESEL";
+                return;
+            }
+            if (doctorsTable.Where(d => d.PESEL == pesel).ToList().Count == 1)
+            {
+                InvalidData_box.Text = "Lekarz z takim numerem PESEL już jest w systemie";
+                return;
+            }
+            string phone = DocPhone_input.Text;
+            if (phone == "" || phone == "Phone")
+            {
+                InvalidData_box.Text = "Proszę podać prawidłowy numer telefonu";
+                return;
+            }
+            string idspec = IdSpec_input.Text;
+            int idValue = int.Parse(idspec);
+            if (specTable.Where(s => s.idSpec == idValue).ToList().Count != 1)
+            {
+                InvalidData_box.Text = "Nie ma takiej specjalizacj";
+                return;
+            }
+
+            Doctors doctor = new Doctors()
+            {
+                FirstName = DocName_input.Text,
+                LastName = DocLastName_input.Text,
+                PESEL = DocPESEL_input.Text,
+                phoneNb = DocPhone_input.Text,
+                mail = DocEmail_input.Text,
+                specializations = idValue
+            };
+            
+           
+            
+            db.Doctors.Add(doctor);
+            db.SaveChanges();
+            Clear();
+            dostorsList();
+            MessageBox.Show("Dodano nowego lekarza");
+        }
+
+
+        private void dostorsList()
+        {
+            var db = new PrzychodniaEntities();
+            doctorsDataGrid.AutoGenerateColumns = false;
+            doctorsDataGrid.ItemsSource = db.Doctors.ToList();
         }
 
         private void MainMenu_btn_Click(object sender, RoutedEventArgs e)
@@ -37,52 +121,23 @@ namespace HospitalProjektWPF
             objMainMenu.Show();
         }
 
-        void Clear()
-        {
-            DocName_input.Text = DocLastName_input.Text = DocPESEL_input.Text = DocPhone_input.Text = DocEmail_input.Text = IdSpec_input.Text = "";
-        }
+
+        //private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        //{
+        //    DataGridRow row = sender as DataGridRow;
+        //    var db = new PrzychodniaEntities();
+        //    var doctor = new Doctors();
+        //    doctor.idDoc = Convert.ToInt32(row.idDoc.value);
+        //    doctor = db.Doctors.Where(d => d.idDoc == doctor.idDoc);
+        //    DocName_input.Text = doctor.FirstName;
+        //    DocLastName_input.Text = doctor.LastName;
+        //    DocPESEL_input.Text = doctor.PESEL;
+        //    DocPhone_input.Text = doctor.phoneNb;
+        //    DocEmail_input.Text = doctor.mail;
+        //    IdSpec_input.Text = doctor.specializations;
 
 
-        private void Grid_Loaded(object sender, RoutedEventArgs e)
-        {
-            Clear();
-        }
 
-        private void AddDoc_btn_Click(object sender, RoutedEventArgs e)
-        {   
-
-            doctor.FirstName = DocName_input.Text;
-            doctor.LastName = DocLastName_input.Text;
-            doctor.PESEL = DocPESEL_input.Text;
-            doctor.phoneNb = DocPhone_input.Text;
-            doctor.mail = DocEmail_input.Text;
-            var idspec = IdSpec_input.Text;
-            int idValue = int.Parse(idspec);
-            doctor.specializations = idValue;
-
-            using (PrzychodniaEntities db = new PrzychodniaEntities())
-            {
-                db.Doctors.Add(doctor);
-                db.SaveChanges();
-            }
-            MessageBox.Show("Dodano nowego lekarza");
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-
-            System.Windows.Data.CollectionViewSource przychodniaEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("przychodniaEntitiesViewSource")));
-            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
-            // przychodniaEntitiesViewSource.Źródło = [ogólne źródło danych]
-        }
-        private void dostorsList()
-        {
-            using (PrzychodniaEntities db = new PrzychodniaEntities())
-            {
-                doctorsDataGrid.ItemsSource = db.Doctors.ToList();
-            }
-              
-        }
-
+        //}
     }
 }
--- HospitalProjektWPF/PatientsWindow.xaml.cs
+++ HospitalProjektWPF/PatientsWindow.xaml.cs
@@ -22,13 +22,93 @@ namespace HospitalProjektWPF
         public PatientsWindow()
         {
             InitializeComponent();
+            patientsList();
         }
 
+
+        void Clear()
+        {
+            Name_input.Text = LastName_input.Text = PESEL_input.Text = Phone_input.Text = email_input.Text = PostalCode_input.Text = City_input.Text = Street_input.Text=Gender_input.Text = "";
+        }
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            System.Windows.Data.CollectionViewSource przychodniaEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("przychodniaEntitiesViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // przychodniaEntitiesViewSource.Źródło = [ogólne źródło danych]
+        }
+
+        private void AddPatient_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new PrzychodniaEntities();
+            PatInvalidData_box.Text = "";
+            var patientsTable = db.Patients;
+            string name = Name_input.Text;
+            if (name == "")
+            {
+                PatInvalidData_box.Text = "Proszę podać imę!";
+                return;
+            }
+            string lastName = LastName_input.Text;
+            if (lastName == "")
+            {
+                PatInvalidData_box.Text = "Proszę podać nazwisko!";
+                return;
+            }
+            string pesel = PESEL_input.Text;
+            if (pesel == "" || pesel.Length != 11)
+            {
+                PatInvalidData_box.Text = "Proszę podać prawidłowy PESEL";
+                return;
+            }
+            if (patientsTable.Where(d => d.PESEL == pesel).ToList().Count == 1)
+            {
+                PatInvalidData_box.Text = "Lekarz z takim numerem PESEL już jest w systemie";
+                return;
+            }
+            string phone = Phone_input.Text;
+            if (phone == "" || phone == "Phone")
+            {
+                PatInvalidData_box.Text = "Proszę podać prawidłowy numer telefonu";
+                return;
+            }
+         
+
+            Patients patient = new Patients()
+            {
+                FirstName = Name_input.Text,
+                LastName = LastName_input.Text,
+                PESEL = PESEL_input.Text,
+                phoneNb = Phone_input.Text,
+                mail = email_input.Text,
+                gender = Gender_input.Text,
+                city = City_input.Text,
+                birthDate = PatBirthDate_picker.SelectedDate.Value,
+                postalCode = PostalCode_input.Text,
+                street_adress = Street_input.Text,
+
+                
+            };
+
+
+
+            db.Patients.Add(patient);
+            db.SaveChanges();
+            Clear();
+            patientsList();
+            MessageBox.Show("Dodano nowego lekarza");
+        }
         private void MainMenu_btn_Click(object sender, RoutedEventArgs e)
         {
             MainMenu objMainMenu = new MainMenu();
             this.Visibility = Visibility.Hidden;
             objMainMenu.Show();
+        }
+        void patientsList()
+        {
+            var db = new PrzychodniaEntities();
+            patientsDataGrid.AutoGenerateColumns = false;
+            patientsDataGrid.ItemsSource = db.Patients.ToList();
         }
     }
 }
