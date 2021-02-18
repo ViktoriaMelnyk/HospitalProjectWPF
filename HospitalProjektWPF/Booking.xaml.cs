148e491a28c22fa5ff47718fe4543af1361a214e
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-02-18 17:39:59 +0100

    Bookig Visit  window done , small changes , added Instalator

--- HospitalProjektWPF/Booking.xaml.cs
+++ HospitalProjektWPF/Booking.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace HospitalProjektWPF
 {
@@ -22,13 +12,129 @@ namespace HospitalProjektWPF
         public Booking()
         {
             InitializeComponent();
+            reservationsList();
+        }
+        void Clear()
+        {
+            IdPat_input.Text = IdDoc_input.Text = dateOfVisit.Text = TimeOfVisit.Text = "";
         }
 
+
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            Clear();
+            System.Windows.Data.CollectionViewSource przychodniaEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("przychodniaEntitiesViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // przychodniaEntitiesViewSource.Źródło = [ogólne źródło danych]
+        }
+        void reservationsList()
+        {
+            var db = new PrzychodniaEntities();
+            bookingVisitDataGrid.AutoGenerateColumns = false;
+            var reservationsTable = db.BookingVisit.ToList().Select(a => new
+            {
+                idBV = a.idBV,
+                idDoc = a.idDoc,
+                idPat = a.idPat,
+                dateOfReserv = a.dateOfReserv.Value.ToString("dd-MM-yyyy"),
+                timeOfVisit = a.timeOfVisit.ToString(),
+            }).ToList();
+            bookingVisitDataGrid.ItemsSource = reservationsTable;
+        }
         private void MainMenu_btn_Click(object sender, RoutedEventArgs e)
         {
             MainMenu objMainMenu = new MainMenu();
             this.Visibility = Visibility.Hidden;
             objMainMenu.Show();
+        }
+
+        private void Reserv_btn_Click(object sender, RoutedEventArgs e)
+        {
+            var db = new PrzychodniaEntities();
+            ReservInvalidData_box.Text = "";
+
+
+
+            string idPat = IdPat_input.Text;
+            int idValue = int.Parse(idPat);
+            if (idPat == "")
+            {
+                ReservInvalidData_box.Text = "Proszę podać id pacjenta";
+                return;
+            }
+            if (db.Patients.Where(i => i.idPat == idValue).ToList().Count != 1)
+            {
+                ReservInvalidData_box.Text = "Pacjent z takiem id nie jest zarajestrowany";
+                return;
+            }
+            string idDoc = IdDoc_input.Text;
+            int idValue2 = int.Parse(idDoc);
+            if (idDoc == "")
+            {
+                ReservInvalidData_box.Text = "Proszę podać id lekarza";
+                return;
+            }
+            var doctors = db.Doctors.Where(d => d.idDoc == idValue2).ToList();
+            var wszyscy = db.Doctors.ToList();
+            if (doctors.Count() != 1)
+            {
+                ReservInvalidData_box.Text = "Lekarz z takiem id nie pracuje w tej przychodni";
+                return;
+            }
+            if (dateOfVisit.SelectedDate == null)
+            {
+                ReservInvalidData_box.Text = "Proszę podać datę wyzyty";
+                return;
+            }
+            string time = TimeOfVisit.Text;
+            if (time == "")
+            {
+                ReservInvalidData_box.Text = "Proszę podać godzinę wyzyty";
+                return;
+            }
+
+            DateTime dt = (dateOfVisit.SelectedDate is null) ? new DateTime(0, 0, 0) : (DateTime)dateOfVisit.SelectedDate;
+            dt += TimeSpan.Parse(time);
+            int doc = int.Parse(idDoc);
+
+            var from = dt - new TimeSpan(0, 15, 0);
+            var to = dt + new TimeSpan(0, 15, 0);
+            var exist = db.BookingVisit
+                .ToList()
+                .Where(a =>
+                    a.idDoc == doc &&
+                    a.dateOfReserv + a.timeOfVisit > from &&
+                    a.dateOfReserv + a.timeOfVisit < to
+                );
+
+            if (exist.FirstOrDefault() != null)
+            {
+                ReservInvalidData_box.Text = "Lekarz z takim indeksem już jest zajęty w tym terminie";
+                return;
+            }
+
+
+
+
+
+            BookingVisit reservation = new BookingVisit()
+            {
+                idPat = int.Parse(IdPat_input.Text),
+                idDoc = int.Parse(IdDoc_input.Text),
+                dateOfReserv = dateOfVisit.SelectedDate.Value,
+                timeOfVisit = TimeSpan.Parse(TimeOfVisit.Text),
+
+
+            };
+
+
+
+            db.BookingVisit.Add(reservation);
+            db.SaveChanges();
+            Clear();
+            reservationsList();
+            MessageBox.Show("Zerezerwowano");
         }
     }
 }
