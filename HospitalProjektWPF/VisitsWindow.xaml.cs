262a052d1db6bfb50b963ae50c7d3184ba5b3571
ViktoriaMelnyk <oliviatvist12@gmail.com>
2021-02-18 13:36:51 +0100

    Visits Window done

--- HospitalProjektWPF/VisitsWindow.xaml.cs
+++ HospitalProjektWPF/VisitsWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
 
 namespace HospitalProjektWPF
 {
@@ -22,8 +23,38 @@ namespace HospitalProjektWPF
         public VisitsWindow()
         {
             InitializeComponent();
+            visitsList();
         }
 
+      
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+            System.Windows.Data.CollectionViewSource przychodniaEntitiesViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("przychodniaEntitiesViewSource")));
+            // Załaduj dane poprzez ustawienie właściwości CollectionViewSource.Source:
+            // przychodniaEntitiesViewSource.Źródło = [ogólne źródło danych]
+            
+        }
+        void visitsList()
+        {
+            var db = new PrzychodniaEntities();
+            visitsDataGrid.AutoGenerateColumns = false;
+            var visitsTable = db.Visits.ToList().Select(a => new {
+                idVis = a.idVis,
+                idPat = a.idPat,
+                idDoc = a.idDoc,
+                DateOfVisit = a.DateOfVisit.Value.ToString("dd-MM-yyyy"),
+                TimeOfVisit = a.TimeOfVisit.ToString(),
+                Symptoms= a.Symptoms,
+                Diagnosis= a.Diagnosis,
+                idDis= a.idDis,
+                idPrescr= a.idPrescr,
+                sickLeave= a.sickLeave,
+                recommendations= a.recommendations,
+                }).ToList();
+            visitsDataGrid.ItemsSource = visitsTable;
+        }
         private void MainMenu_btn_Click(object sender, RoutedEventArgs e)
         {
             MainMenu objMainMenu = new MainMenu();
